7ed1453af763178bc57fb74154c7c486f9effd3e
Angel <angelmaroru@gmail.com>
2026-02-18 03:35:48 +0100

    subida completa iconos

--- water-pwa/vite.config.ts
+++ water-pwa/vite.config.ts
@@ -16,8 +16,8 @@ export default defineConfig({
         background_color: "#ffffff",
         theme_color: "#ffffff",
         icons: [
-          { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }
+          { src: "/public/icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "/public/icon-512.png", sizes: "512x512", type: "image/png" }
         ]
       }
     })
